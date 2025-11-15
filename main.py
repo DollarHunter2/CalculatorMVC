536d0480c7f3aca07832b530211d7c97673407aa
Joe <codecampaj@gmail.com>
2025-11-15 16:21:25 +0000

    calculator logo

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from PIL import Image, ImageTk
 
 def main():
     root = tk.Tk()
-
-    root.geometry("400x600")
     root.resizable(False, False)
     icon = ImageTk.PhotoImage (Image.open(r"C:\Users\Cellusys CodeCamp\Desktop\Joe(vs project)\Scientific-calculator-MVC-structure-\assets sy\calculator image.png"))
     root.iconphoto(False, icon)
