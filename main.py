827ef0a581aa434623b348b4b7ea16f8f368aed7
joecode-camp1 <codecampaj@gmail.com>
2025-12-09 18:11:56 +0000

    Remove main function and tkinter setup

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import tkinter as tk
-from calculator_controller import CalculatorController
-from PIL import Image, ImageTk
 
-def main():
-    root = tk.Tk()
-    root.resizable(False, False)
-    icon = ImageTk.PhotoImage (Image.open(r"C:\Users\Cellusys CodeCamp\Desktop\Joe(vs project)\Scientific-calculator-MVC-structure-\assets sy\calculator image.png"))
-    root.iconphoto(False, icon)
-    app = CalculatorController(root)
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
